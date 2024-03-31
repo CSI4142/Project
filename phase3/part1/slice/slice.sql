23f9103f8034489136dccfbb1f562a39a3671e2a
Dhara Patel <>
2024-03-31 16:10:39 -0400

    Edited slice query to include 1 dimension

--- phase3/part1/slice/slice.sql
+++ phase3/part1/slice/slice.sql
@@ -3,6 +3,7 @@
 SELECT
     bp.complain, -- Whether the customer has lodged a complaint (1 for Yes, 0 for No)
     bp.active_member, -- Whether the customer is an active member (1 for Yes, 0 for No)
+	bp.products_number, -- Number of products owned by the customer
     COUNT(*) AS total_customers, -- Total number of customers in the group
     SUM(cft.churn) AS churned_customers, -- Total number of churned customers in the group
     (SUM(cft.churn)::decimal / COUNT(*)) * 100 AS churn_rate -- Churn rate calculated as a percentage
@@ -16,7 +17,9 @@ WHERE
     loc.country = 'France' -- Filter by customers located in France
 GROUP BY
     bp.complain, -- Grouping by complaints
-    bp.active_member -- Grouping by active members
+    bp.active_member, -- Grouping by active members
+	bp.products_number -- Grouping by number of products
 ORDER BY
     bp.complain, -- Then ordering by complaints
-    bp.active_member; -- Ordering by active members
+    bp.active_member, -- Ordering by active members
+	bp.products_number; -- Ordering by number of products
