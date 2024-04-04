2fc52b4abbd9d7253bbbf33da80f69c8c063284a
Oles Turchyn <52382133+OlesTurchyn@users.noreply.github.com>
2024-04-04 00:03:01 -0400

    Added to Q4 (women)

--- phase3/part1/combined_olap_ops/q4.sql
+++ phase3/part1/combined_olap_ops/q4.sql
@@ -1,8 +1,8 @@
 -- Combined OLAP Query 4
--- Compare the churn rate for young males (18-30) between France and Germany. 
+-- Compare the churn rate for young males (18-30) and females (18-30) between France and Germany.
 
 SELECT
-    -- Dimensions for drill down/roll up: country
+    -- Dimensions for drill down/roll up: country and gender
     l.country AS Country,
     cp.gender AS Gender,
     -- Measure for analysis: churn rate
@@ -16,14 +16,14 @@ JOIN
 JOIN
     location_dim l ON cf.location_key = l.location_key
 WHERE
-    -- Dice: Filtering for young males (18-30)
-    cp.gender = 'Male'
+    -- Dice: Filtering for young males (18-30) and females (18-30)
+    (cp.gender = 'Male' OR cp.gender = 'Female')
     AND cp.age BETWEEN 18 AND 30
     -- Slice: Filtering for France and Germany
     AND l.country IN ('France', 'Germany')
 GROUP BY
     -- Grouping by country and gender for analysis
-    l.country, cp.gender
+    GROUPING SETS ((l.country, cp.gender), (l.country))
 ORDER BY
     -- Ordering by country and gender for readability
     l.country, cp.gender;
