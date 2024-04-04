9fb2a7c07f0c60406be45f8e06780655d6149057
Oles Turchyn <52382133+OlesTurchyn@users.noreply.github.com>
2024-04-03 23:52:11 -0400

    Small fixes after more rigorous testing

--- phase3/part1/combined_olap_ops/q3.sql
+++ phase3/part1/combined_olap_ops/q3.sql
@@ -2,7 +2,7 @@
 -- Compare the churn rate for different credit card type holders in Germany
 SELECT
     -- Dimension for drill down/roll up: credit_card_type
-    cp.credit_card_type AS Credit_Card_Type,
+    cpd.credit_card_type AS Credit_Card_Type,
     -- Measure for analysis: churn rate
     ROUND(SUM(CASE WHEN cf.churn = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS Churn_Rate
 FROM
@@ -13,14 +13,16 @@ JOIN
     customer_profile_dim cp ON cf.customer_key = cp.customer_id
 JOIN
     location_dim l ON cf.location_key = l.location_key
+JOIN
+    credit_profile_dim cpd ON cf.credit_profile_key = cpd.credit_profile_key
 WHERE
     -- Slice: Filtering for Germany
     l.country = 'Germany'
     -- Dice: Filtering for specific credit card types
-    AND cp.credit_card_type IN ('silver', 'gold', 'diamond', 'platinum')
+    AND cpd.credit_card_type IN ('SILVER', 'GOLD', 'DIAMOND', 'PLATINUM')
 GROUP BY
     -- Grouping by credit card type for analysis
-    cp.credit_card_type
+    cpd.credit_card_type
 ORDER BY
     -- Ordering by credit card type for readability
-    cp.credit_card_type;
+    cpd.credit_card_type;
